375f8cd3b621c6d87aff503365e2fd7949b04cab
Your Name <aysenur>
2017-06-23 08:36:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aysenur/webview_/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aysenur.webview_;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView= (TextView) findViewById(R.id.tv);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://gelecegiyazanlar.turkcell.com.tr/"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
